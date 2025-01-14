d231fbd308516f571954d677c442379d8b064ac3
Aline Anacleto <143654541+alineanacletoo@users.noreply.github.com>
2025-01-14 13:27:14 -0300

    Update Postagem.service.ts

--- src/postagem/services/Postagem.service.ts
+++ src/postagem/services/Postagem.service.ts
@@ -14,4 +14,53 @@ export class PostagemService{
     async findAll(): Promise<Postagem[]>{
         return this.postagemRepository.find(); // SELECT * FROM tb_postagens;
     }
+
+     async findById(id: number): Promise<Postagem> {
+
+        // SELECT * FROM tb_postagem WHERE id - ?;
+        const postagem = await this.postagemRepository.findOne({
+            where: {
+                id
+            }
+        })
+
+        if(!postagem)
+            throw new HttpException("Postagem não encontrada", HttpStatus.NOT_FOUND)
+        
+        return postagem;
+
+    }
+
+    async findByTitulo(titulo: string): Promise<Postagem[]>{
+        return this.postagemRepository.find({
+            where:{
+                titulo: ILike(`%${titulo}%`)// ILike - Case Insensitive | Like - Case Sensitive
+            }
+        }); 
+    }
+        //gravar a informação no banco de dados
+    async create(postagem: Postagem): Promise<Postagem>{
+
+        //INSERT INTO tb_postagem (titulo, texto) VALUES (?, ?)
+        return await this.postagemRepository.save(postagem);
+
+    }
+
+    async update(postagem: Postagem): Promise<Postagem>{
+        await this.findById(postagem.id)
+
+
+        // UPDATE tb_postagem SET titulo = postagem.titulo,
+        // texto = postagem,texto, data = CURENT_TIMESTAR()
+        // WHERE id = postagem.id
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async delete(id: number): Promise<DeleteResult>{
+
+        await this.findById(Postagem.id)
+
+         // DELETE tb_postagens WHERE id = ?;   
+        return await this.postagemRepository.delete(id)
+    }
 }
