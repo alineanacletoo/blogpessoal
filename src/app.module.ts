a78a34cc0cb1d77f5720a03e5ae1350fcb40d601
alineanacleto <alineanacleto07@gmail.com>
2025-01-14 09:17:53 -0300

    Criação de postagem

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostagemModule } from './postagem/postagem.module';
 
 @Module({
   imports: [
@@ -15,7 +16,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: true,
       logging: true,
-    })
+    }),
+    PostagemModule,
   ],
   controllers: [],
   providers: [],
--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,18 +1,18 @@
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity({name: "tb_postagens"})
+@Entity({name: "tb_postagens"}) // CREAT TABLE tb_postagem()
 export class Postagem{
 
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn() // AUTO_INCREMENT PRIMARY KEY
     id: number;
 
     @IsNotEmpty() //validação dados do objetos
-    @Column({length: 100, nullable:false})
+    @Column({length: 100, nullable:false}) // VARCHAR(100) NOT NULL
     titulo: string;
     
     @IsNotEmpty() //validação dados do objetos
-    @Column({length: 1000, nullable:false})
+    @Column({length: 1000, nullable:false}) // VARCHAR(1000) NOT NULL
     texto: string;
 
     @UpdateDateColumn()
